b6a52cc06d895699deada60e95e6722e3fc838b3
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-01-14 06:38:38 +0200

    config.mk: minor changes

--- config.mk
+++ config.mk
@@ -1,17 +1,16 @@
-VERSION       = 6.0
+# pkgman version
+VERSION = 6.0
 
-PREFIX        ?= /usr/local
-MANPREFIX     ?= ${PREFIX}/share/man
-LOCALSTATEDIR ?= /var
-SYSCONFDIR    ?= /etc
+# paths
+PREFIX = /usr/local
+MANDIR = ${PREFIX}/share/man
+LOCALSTATEDIR = /var
+SYSCONFDIR = /etc
 
+# flags
 CXXFLAGS += -Wall -Wextra -Wconversion -Wcast-align -Wunused -Wshadow \
 	    -Wcast-align -Wold-style-cast -std=c++17
-
-CPPFLAGS += -DVERSION=\"${VERSION}\" \
-	    -DLOCALSTATEDIR=\"${LOCALSTATEDIR}\" \
-	    -DSYSCONFDIR=\"${SYSCONFDIR}\" \
-	    -DPREFIX=\"${PREFIX}\" \
+CPPFLAGS += -DVERSION=\"${VERSION}\" -DLOCALSTATEDIR=\"${LOCALSTATEDIR}\" \
+	    -DSYSCONFDIR=\"${SYSCONFDIR}\" -DPREFIX=\"${PREFIX}\" \
 	    -DNDEBUG
-
-LDFLAGS   = -lstdc++fs
+LDFLAGS  += -lstdc++fs
