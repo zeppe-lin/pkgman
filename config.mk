c48abb69e60bf2b1be2ee1c85e7261132537d77a
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-11-17 02:13:31 +0200

    restyle config.mk

--- config.mk
+++ config.mk
@@ -1,3 +1,6 @@
+# This file is a part of pkgman.
+# See COPYING and COPYRIGHT files for corresponding information.
+
 # pkgman version
 VERSION = 6.0
 
@@ -7,7 +10,14 @@ BINDIR = ${PREFIX}/bin
 MANDIR = ${PREFIX}/share/man
 
 # flags
-CXXFLAGS += -Wall -Wextra -Wconversion -Wcast-align -Wunused -Wshadow \
-	    -Wcast-align -Wold-style-cast -std=c++17
-CPPFLAGS += -DVERSION=\"${VERSION}\" -DNDEBUG
-LDFLAGS  += -lstdc++fs
+CXXFLAGS = -Wall -Wextra -Wconversion -Wcast-align -Wunused \
+	    -Wshadow -Wcast-align -Wold-style-cast -std=c++17
+CPPFLAGS = -DVERSION=\"${VERSION}\" -DNDEBUG
+LDFLAGS  = -lstdc++fs
+
+# compiler and linker
+CXX = g++
+LD  = ${CXX}
+
+# vim:cc=72:tw=70
+# End of file.
