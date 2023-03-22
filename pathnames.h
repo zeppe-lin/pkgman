5b0b9f5c222c5de2e006151765662b6aa39d593d
Alexandr Savca <alexandr.savca89@gmail.com>
2023-03-22 19:38:39 +0200

    pathnames.h: add comments for definitions

--- pathnames.h
+++ pathnames.h
@@ -1,14 +1,31 @@
 //! \file      pathnames.h
 //! \brief     Pathnames local to the pkgman
 
+//!< Default location for pkgman configuration.
 #define PATH_CONFIG         "/etc/pkgman.conf"
+
+//!< Default location for the package database.
 #define PATH_PKG_DB         "/var/lib/pkg/db"
+
+//!< Default location for the locked packages.
 #define PATH_LOCK_DB        "/var/lib/pkg/locked"
+
+//!< Default location for man(1) executable.
 #define PATH_MAN_BIN        "/usr/bin/man"
+
+//!< Default location for pkgmk(8) executable.
 #define PATH_PKGMK_BIN      "/usr/sbin/pkgmk"
+
+//!< Default location for pkgmk(8) configuration.
 #define PATH_PKGMK_CONFIG   "/etc/pkgmk.conf"
+
+//!< Default location for pkgadd(8) executable.
 #define PATH_PKGADD_BIN     "/usr/sbin/pkgadd"
+
+//!< Default location for pkgadd(8) configuration.
 #define PATH_PKGADD_CONFIG  "/etc/pkgadd.conf"
+
+//!< Default location for pkgrm(8) executable.
 #define PATH_PKGRM_BIN      "/usr/sbin/pkgrm"
 
 // vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
