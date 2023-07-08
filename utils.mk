82e2a8721d0a41a7f1ff1ffdf5170fcadbd2351b
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 08:22:58 +0300

    utils.mk: urlcodes -> deadlinks

--- utils.mk
+++ utils.mk
@@ -2,8 +2,8 @@ GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
 FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
 MAXLINE = 80
 
-urlcodes:
-	@echo "=======> Check URLs for response code"
+deadlinks:
+	@echo "=======> Check for dead links"
 	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT}  \
 		| xargs -P10 -I{} curl -o /dev/null    \
 		 -sw "[%{http_code}] %{url}\n" '{}'    \
@@ -28,4 +28,4 @@ longlines:
 	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
 		'length > ML { print FILENAME ":" FNR " " $$0 }'  {} \;
 
-.PHONY: urlcodes podchecker shellcheck cppcheck longlines
+.PHONY: deadlinks podchecker shellcheck cppcheck longlines
