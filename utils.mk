ef44d18eb2566daf73c095362ed317c00ca5dc61
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 06:00:51 +0300

    utils.mk: optimize a bit

--- utils.mk
+++ utils.mk
@@ -1,8 +1,10 @@
+GREP_DEFS = --exclude-dir=.git --exclude-dir=.github -R .
+
 urlcodes:
 	@echo "=======> Check URLs for response code"
-	@grep -Eiho "https?://[^\"\\'> ]+" *.*      \
-		| xargs -P10 -I{} curl -o /dev/null \
-		 -sw "[%{http_code}] %{url}\n" '{}' \
+	@grep -Eiho "https?://[^\"\\'> ]+" ${GREP_DEFS}  \
+		| xargs -P10 -I{} curl -o /dev/null      \
+		 -sw "[%{http_code}] %{url}\n" '{}'      \
 		| sort -u
 
 podchecker:
@@ -11,13 +13,12 @@ podchecker:
 
 shellcheck:
 	@echo "=======> Check shell scripts for syntax errors"
-	@grep -m1 -l '^#\s*!/bin/sh' -R .                 \
-		 --exclude-dir=.git --exclude-dir=.github \
+	@grep -m1 -l '^#\s*!/bin/sh' ${GREP_DEFS}        \
 		| xargs -L10 shellcheck -s sh
 
 cppcheck:
 	@echo "=======> Static C/C++ code analysis"
-	@cppcheck --enable=all --check-level=exhaustive   \
+	@cppcheck --enable=all --check-level=exhaustive  \
 		--suppress=missingIncludeSystem ${CURDIR}
 
 .PHONY: urlcodes podchecker shellcheck cppcheck
