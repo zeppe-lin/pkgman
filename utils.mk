b2692aa07d1cf3f13d279f5b6aa4fb6978659eac
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 05:46:13 +0300

    add utils.mk

---
+++ utils.mk
@@ -0,0 +1,18 @@
+urlcodes:
+	@echo "=======> Check URLs for response code"
+	@grep -Eiho "https?://[^\"\\'> ]+" *.*      \
+		| xargs -P10 -I{} curl -o /dev/null \
+		 -sw "[%{http_code}] %{url}\n" '{}' \
+		| sort -u
+
+podchecker:
+	@echo "=======> Check PODs for syntax errors"
+	@podchecker *.pod
+
+shellcheck:
+	@echo "=======> Check shell scripts for syntax errors"
+	@grep -m1 -l '^#\s*!/bin/sh' -R .                 \
+		 --exclude-dir=.git --exclude-dir=.github \
+		| xargs -L10 shellcheck -s sh
+
+.PHONY: urlcodes podchecker shellcheck
