7ab1658f3d964dfcbed096c91d2050cdbb778223
Alexandr Savca <alexandr.savca89@gmail.com>
2023-09-10 19:07:53 +0300

    config.mk: remove redundant vars, just use make CXX=c++ for example

--- config.mk
+++ config.mk
@@ -13,7 +13,3 @@ CPPFLAGS    = -DNDEBUG -DVERSION=\"${VERSION}\"
 CXXFLAGS    = -std=c++17 -Wall -Wextra -Wconversion -Wcast-align \
 	      -Wunused -Wshadow -Wold-style-cast
 LDFLAGS     = -lstdc++fs
-
-# compiler and linker
-CXX         = c++
-LD          = ${CXX}
