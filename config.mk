7288076dc7e5cdf97a27541668faf004ce21eb0e
Alexandr Savca <alexandr.savca89@gmail.com>
2024-01-13 14:18:20 +0200

    config.mk: add debug flags via DEBUG=1 make argument

--- config.mk
+++ config.mk
@@ -10,6 +10,12 @@ BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 
 # flags
 CPPFLAGS    = -DNDEBUG -DVERSION=\"${VERSION}\"
+ifeq (${DEBUG}, 1)
+CXXFLAGS    = -std=c++17 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
+              -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
+LDFLAGS     = -lstdc++fs ${CXXFLAGS} -lasan -lubsan
+else
 CXXFLAGS    = -std=c++17 -Wall -Wextra -Wconversion -Wcast-align \
 	      -Wunused -Wshadow -Wold-style-cast
 LDFLAGS     = -lstdc++fs
+endif
